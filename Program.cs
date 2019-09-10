572fe8fe46f6bf5aae9bbe6c045f51badb7bb642
Juan Camilo <juanus196@gmail.com>
2019-09-10 10:23:01 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace sql
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string cs = "Data Source=DESKTOP-O3RV5AC;Initial Catalog=WideWorldImporters; Integrated Security = True";
+            try
+            {
+                using (SqlConnection con = new SqlConnection(cs))
+                {
+                    con.Open();
+                    //Ejecutar procedimiento almacenado desde C#
+                    SqlCommand cmd = new SqlCommand("EXECUTE SaleStockHoldingUpdates2 2998452,80, 10, 5", con);
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    con.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        
+
+        
+    }
+}
