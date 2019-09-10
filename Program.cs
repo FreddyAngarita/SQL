2c596fadb498e6eba3ce86df2efd3466115778e1
Juan Camilo <juanus196@gmail.com>
2019-09-10 10:24:26 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace objetosNativos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cs = "Data Source=DESKTOP-O3RV5AC;Initial Catalog=WideWorldImporters; Integrated Security = True";
+            try
+            {
+                using (SqlConnection con = new SqlConnection(cs))
+                {
+                    //Abrimos la conexión con SQL
+                    con.Open();
+                    //Creamos un dataAdaptar y como parametro le pasamos una sentencia SQL y el objeto de conexión
+                    SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Application.People", con);
+                    //Creamos un DataSet
+                    DataSet ds = new DataSet();
+                    //El método Fill guarda los datos de la sentencia SQL en el DataSet
+                    da.Fill(ds, "Clientes"); // , "Clientes" es opcional, da.fill(ds); es posible.
+
+
+                    con.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
