43551eb4cdf8291a6680464206ae7addd34ce0fa
Juan Camilo <juanus196@gmail.com>
2019-09-10 10:14:39 -0500

    Rename Try Catch Acid.sql to Stored procedure/Try Catch Acid.sql

--- Stored procedure/Try Catch Acid.sql
+++ Stored procedure/Try Catch Acid.sql
@@ -2,13 +2,13 @@
 -- Author:		<Juan Camilo Urrego Serna, Freddy Angarita>
 -- Description:	<Description: Observamos el uso de TRY, CATCH para crear una transaccion ACID>
 -- =============================================
---Creamos un procedimimiento almacenado para actualizar el stock despuÈs de una venta
---Este procedimiento contiene una transacciÛn, verifica que todas las transacciones
+--Creamos un procedimimiento almacenado para actualizar el stock despu√©s de una venta
+--Este procedimiento contiene una transacci√≥n, verifica que todas las transacciones
 --Se ejecuten correctamente o no ejecuta ninguna
 CREATE PROCEDURE SaleStockHoldingUpdates2(
 	@StockItemTransactionID INT,
 	@ItemId INT,--(Item a vender)
-	@TransactionTypeID INT,--Tipo de transacciÛn (10 para vender)
+	@TransactionTypeID INT,--Tipo de transacci√≥n (10 para vender)
 	@Quantity DECIMAL(18,3)--Cantidad a vender
 )
 AS
@@ -17,11 +17,11 @@ BEGIN
 		BEGIN TRANSACTION
 
 		DECLARE @qoh INT--Stock en el inventario
-		--TransacciÛn 1
+		--Transacci√≥n 1
 		SELECT @qoh = QuantityOnHand 
 		FROM Warehouse.StockItemHoldings
 		WHERE StockItemID = @ItemId
-		--TransacciÛn 2
+		--Transacci√≥n 2
 		INSERT INTO [Warehouse].[StockItemTransactions]
            ([StockItemTransactionID]
            ,[StockItemID]
@@ -36,8 +36,8 @@ BEGIN
            ,[LastEditedWhen])
      VALUES
            (@StockItemTransactionID
-           ,@ItemId--Item que se vendiÛ
-           ,@TransactionTypeID--Tipo de transacciÛn (10 es venta)
+           ,@ItemId--Item que se vendi√≥
+           ,@TransactionTypeID--Tipo de transacci√≥n (10 es venta)
            ,NULL
            ,NULL
            ,NULL
@@ -46,17 +46,17 @@ BEGIN
            ,@Quantity--Cantidad vendida
            ,1
            ,GETDATE())
-		--TransacciÛn 3
-		UPDATE Warehouse.StockItemHoldings --Modifica stock del producto que se vendiÛ
-		SET QuantityOnHand = QuantityOnHand - @Quantity --Cantidad actual menos la que se vendiÛ
+		--Transacci√≥n 3
+		UPDATE Warehouse.StockItemHoldings --Modifica stock del producto que se vendi√≥
+		SET QuantityOnHand = QuantityOnHand - @Quantity --Cantidad actual menos la que se vendi√≥
 		WHERE StockItemID = @ItemId
 
-		COMMIT TRANSACTION --Confirmamos la transacciÛn
+		COMMIT TRANSACTION --Confirmamos la transacci√≥n
 		--Si alguna de las 3 transacciones falla, no se ejecuta ninguna
 	END TRY
 		BEGIN CATCH
-			ROLLBACK TRANSACTION--Hace Rollback si alguna transacciÛn falla
-			RAISERROR('No se puede completar la transacciÛn', 16, 1)
+			ROLLBACK TRANSACTION--Hace Rollback si alguna transacci√≥n falla
+			RAISERROR('No se puede completar la transacci√≥n', 16, 1)
 		END CATCH
 END
 GO
@@ -69,12 +69,12 @@ WHERE StockItemID = 80
 --Vendemos 5 unidades del producto 80
 EXECUTE SaleStockHoldingUpdates2 29000,80, 10, 5
 
---Verificamos que efectivamente el stock se modificÛ correctamente. 
+--Verificamos que efectivamente el stock se modific√≥ correctamente. 
 SELECT StockItemID, QuantityOnHand
 FROM Warehouse.StockItemHoldings
 WHERE StockItemID = 80
 
-----Id repetido, lanzar· errpr y no se ejecuta ninguna transacciÛn
+----Id repetido, lanzar√° errpr y no se ejecuta ninguna transacci√≥n
 EXECUTE SaleStockHoldingUpdates2 29000,80, 10, 5
 
 --Verificamos que el stock se mantuvo igual.
